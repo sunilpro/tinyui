c7676a7e055fac92ff236f711966e09e1fb54a89
Sunil Reddy <sunilpro@gmail.com>
2019-05-29 19:14:19 +0800

    Move to github pages

--- examples/server.js
+++ examples/server.js
@@ -1,6 +1,20 @@
-var express = require("express");
+const http2 = require('http2')
+var express = require("express")
+var compression = require("shrink-ray-current")
  
 var app = express();
+
+app.use(compression({filter: shouldCompress}))
+
+function shouldCompress(req, res) {
+  //console.log(req.headers['accept-encoding'])
+  if (req.headers['accept-encoding'].includes("gzip")) {
+    return true
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 express.static.mime.types['data'] = 'multipart/bag'
 express.static.mime.types['wasm'] = 'application/wasm' 
